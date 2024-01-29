a462057c37b3f3093c266aac2b2eb534d6ea7d43
kenzo-b <contact@kbriche.fr>
2024-01-29 17:06:33 +0100

    new function CITBM and more detail in function doc

--- IPv4/__init__.py
+++ IPv4/__init__.py
@@ -23,21 +23,22 @@ def IsMask(mask):
 
 
 def DTB(dec_p):
-    """take a decimal pointed form and return it binary pointed form"""
+    """take a decimal pointed form and return it binary pointed form -> bin.bin.bin.bin"""
 
     bin_p = '.'.join([bin(int(octet))[2:].zfill(8) for octet in dec_p.split('.')])
     return bin_p
 
 
 def BTD(bin_p):
-    """take a binary pointed form and return it decimal pointed form"""
+    """take a binary pointed form and return it decimal pointed form -> dec.dec.dec.dec"""
 
     dec_p = '.'.join(str(int(octet, 2)) for octet in bin_p.split('.'))
     return dec_p
 
 
 def NetIp(ip, mask):
-    """Calculate a network IPv4 with an AND operation and return both dec and bin form"""
+    """Calculate a network IPv4 with an AND operation and return both dec and bin form
+    -> bin.bin.bin.bin <=> dec.dec.dec.dec"""
 
     str_bin_ip = ''.join(DTB(ip).split('.'))
     str_bin_subnet_mask = ''.join((DTB(mask)).split('.'))
@@ -50,7 +51,7 @@ def NetIp(ip, mask):
 
 
 def CIP(ip):
-    """take a dec ip and return the char of it ip classe"""
+    """take a dec ip and return the char of it ip classe -> "X" """
 
     classe_str = "ABCDE"
     first_octet = DTB(ip).split('.')[0]
@@ -61,11 +62,23 @@ def CIP(ip):
             return classe_str[4]
 
 
-def CTBM(cidr):
-    """take a cidr notation string and return a binary mask string -> n.n.n.n"""
+def CITBM(cidr):
+    """take a cidr notation string and return a binary mask string -> bin.bin.bin.bin"""
 
     o_bit_nb = int(cidr.split("/")[1])
     z_bit_nb = 32 - int(o_bit_nb)
     str_bin_mask = "" + "1" * o_bit_nb + "0" * z_bit_nb
     bin_mask = ".".join(str_bin_mask[i:i + 8] for i in range(0, len(str_bin_mask), 8))
     return bin_mask
+
+
+def CTBM(classe_ip):
+    """take an ip classe and return a binary mask string -> bin.bin.bin.bin"""
+
+    classe = {"A": 8, "B": 16, "C": 24, "D": 28, "E": 28}
+    for key, value in classe.items():
+        if classe_ip == key:
+            str_bin_mask = "" + "1" * value + "0" * (32 - value)
+            bin_mask = ".".join(str_bin_mask[i:i + 8] for i in range(0, len(str_bin_mask), 8))
+            return bin_mask
+
